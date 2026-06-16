ff6ee729eb6465a170074522d0872cdeb15f5963
Rahman Ardi Saputra <rahmansuwarto@gmail.com>
2026-06-16 09:34:50 +0700

    perbaikan barcode

--- resources/views/barang/show.blade.php
+++ resources/views/barang/show.blade.php
@@ -96,7 +96,14 @@
                 <div id="label-qr" class="border p-3 mb-3 bg-white text-start" style="width: 300px; margin: 0 auto;">
                     <!-- QR Code SVG Inline -->
                     <div class="text-center mb-2">
-                        {!! QrCode::size(200)->generate($barang->kode_aset) !!}
+                        @php
+    $isiQr = "No Inv: {$barang->kode_aset}\n" .
+             "Nama: {$barang->merek} {$barang->jenis}\n" .
+             "Lokasi: " . ($barang->lokasi->nama_ruangan ?? '-') . "\n" .
+             "Tgl. Perolehan: " . \Carbon\Carbon::parse($barang->tanggal_perolehan)->format('d-m-Y');
+@endphp
+
+{!! QrCode::size(200)->generate($isiQr) !!}
                     </div>
                     
                     <!-- Informasi Barang di Bawah QR -->
